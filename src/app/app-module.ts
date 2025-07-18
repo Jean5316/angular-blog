7100230782a4505bf362d71cf5ecf6643f36fb19
Jean Carlo <jean160599@gmail.com>
2025-07-18 03:03:14 -0400

    Criação de Componentes e pages com estilização

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { MenuBar } from './components/menu-bar/menu-bar';
+import { MenuTitle } from './components/menu-title/menu-title';
+import { BigCard } from './components/big-card/big-card';
+import { SmallCard } from './components/small-card/small-card';
+import { Home } from './pages/home/home';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    MenuBar,
+    MenuTitle,
+    BigCard,
+    SmallCard,
+    Home
   ],
   imports: [
     BrowserModule,
